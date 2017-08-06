6faa4194fef51718aebed237777f2359206ae73a
suzan2go <ksuzuki180@gmail.com>
2017-08-06 20:32:37 +0900

    jdbc and postgres

--- src/main/java/com/example/demo/MessagesController.java
+++ src/main/java/com/example/demo/MessagesController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,16 +14,21 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @RestController
 @RequestMapping("messages")
 public class MessagesController {
-    final List<Message> messages = new CopyOnWriteArrayList<>();
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Message> getMessages() {
-        return messages;
+        return jdbcTemplate.query("SELECT text From messages ORDER BY id", (rs, i) -> {
+            Message m = new Message();
+            m.setText(rs.getString("text"));
+            return m;
+        });
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Message postMessages(@RequestBody Message message) {
-        messages.add(message);
+        jdbcTemplate.update("INSERT INTO messages(text) VALUES (?)", message.getText());
         return message;
     }
 }
