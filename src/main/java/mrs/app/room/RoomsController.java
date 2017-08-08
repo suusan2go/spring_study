76d5fb63ae9e81c736ad380832b3fad97a270f7c
suzan2go <ksuzuki180@gmail.com>
2017-08-08 23:05:52 +0900

    create rservable_room index

--- src/main/java/mrs/app/room/RoomsController.java
+++ src/main/java/mrs/app/room/RoomsController.java
@@ -3,8 +3,10 @@ package mrs.app.room;
 import mrs.domain.model.ReservableRoom;
 import mrs.domain.service.room.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,6 +24,13 @@ public class RoomsController {
         LocalDate today = LocalDate.now();
         List<ReservableRoom> rooms = roomService.findReservableRooms(today);
         model.addAttribute("date", today);
+        model.addAttribute("rooms", rooms);
+        return "room/listRooms";
+    }
+
+    @RequestMapping(path = "{date}", method = RequestMethod.GET)
+    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date, Model model) {
+        List<ReservableRoom> rooms = roomService.findReservableRooms(date);
         model.addAttribute("rooms", rooms);
         return "room/listRooms";
     }
